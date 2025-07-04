347c8267bbf6b376e756b21767afb24828508f34
aragaki_tutu <aragaki_tutu@example.com>
2025-07-04 14:41:51 +0800

    提交一波

---
+++ entry/src/main/cpp/render.cpp
@@ -0,0 +1,8 @@
+//
+// Created on 2025/7/3.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#include "napi/native_api.h"
+
