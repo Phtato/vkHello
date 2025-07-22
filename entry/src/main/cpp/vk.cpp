8c35d05ca3f0e0a9756ac32bb9b6f1bb8e3464b8
aragaki_tutu <aragaki_tutu@example.com>
2025-07-22 17:02:09 +0800

    提交一波

--- entry/src/main/cpp/vk.cpp
+++ entry/src/main/cpp/vk.cpp
@@ -227,7 +227,7 @@ private:
         createFramebuffers();
         createCommandPool();
         createVertexBuffer();
-        createCommandBuffer();
+        createCommandBuffers();
         createSyncObjects();
     }
 
@@ -885,14 +885,15 @@ private:
         }
     }
 
-    void createCommandBuffer() {
+    void createCommandBuffers() {
         commandBuffers.resize(MAX_FRAMES_IN_FLIGHT);
+
         VkCommandBufferAllocateInfo allocInfo{};
         allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
         allocInfo.commandPool = commandPool;
         allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-        allocInfo.commandBufferCount = 1;
-        allocInfo.commandBufferCount = (uint32_t)commandBuffers.size();
+        allocInfo.commandBufferCount = (uint32_t) commandBuffers.size();
+
         if (vkAllocateCommandBuffers(device, &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
             throw std::runtime_error("failed to allocate command buffers!");
         }
