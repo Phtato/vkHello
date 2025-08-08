6027ec8a91703b2294ab08ad3b68996f5f370bb0
aragaki_tutu <aragaki_tutu@example.com>
2025-08-08 17:42:47 +0800

    旋转的贴图正方形

--- entry/src/main/cpp/vk.cpp
+++ entry/src/main/cpp/vk.cpp
@@ -248,12 +248,16 @@ private:
 
     VkImageView textureImageView;
     VkSampler textureSampler;
-
+/*
+layout(location = 0) in vec2 inPosition;
+layout(location = 1) in vec3 inColor;
+layout(location = 2) in vec2 inTexCoord;
+*/
 const std::vector<Vertex> vertices = {
-    {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-    {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
-    {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
-    {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}}
+    {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+    {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
+    {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+    {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f}}
 };
 
     const std::vector<uint16_t> indices = {0, 1, 2, 2, 3, 0};
